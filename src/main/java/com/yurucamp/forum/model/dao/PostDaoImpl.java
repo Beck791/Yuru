3081c7f1dc9699ad66dfca7c92100152f5299564
poppinpony <poppinpony@gmail.com>
2020-11-05 15:07:26 +0800

    no message

--- src/main/java/com/yurucamp/forum/model/dao/PostDaoImpl.java
+++ src/main/java/com/yurucamp/forum/model/dao/PostDaoImpl.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
-import com.gamebase.article.model.ArticleTitle;
 import com.yurucamp.forum.model.PostBean;
 
 @Repository
