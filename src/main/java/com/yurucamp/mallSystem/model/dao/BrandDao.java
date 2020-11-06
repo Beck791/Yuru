9e3a225a56eb761b5388fd541972d61715fdcfd2
Beck791 <gagawulala791@gmail.com>
2020-11-06 09:21:34 +0800

    update

--- src/main/java/com/yurucamp/mallSystem/model/dao/BrandDao.java
+++ src/main/java/com/yurucamp/mallSystem/model/dao/BrandDao.java
@@ -4,7 +4,6 @@ import java.sql.SQLException;
 import java.util.List;
 
 import com.yurucamp.mallSystem.model.BrandBean;
-import com.yurucamp.mallSystem.model.ProductBean;
 
 public interface BrandDao {
 
