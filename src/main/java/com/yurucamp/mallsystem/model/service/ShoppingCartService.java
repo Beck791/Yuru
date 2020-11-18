45c35e6ceffeb6459bdc576fa3314748c320d77c
Beck791 <gagawulala791@gmail.com>
2020-11-18 11:02:13 +0800

    update

--- src/main/java/com/yurucamp/mallsystem/model/service/ShoppingCartService.java
+++ src/main/java/com/yurucamp/mallsystem/model/service/ShoppingCartService.java
@@ -4,14 +4,20 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.yurucamp.mallsystem.model.ShoppingCart;
 import com.yurucamp.mallsystem.model.dao.ShoppingCartDao;
 
+@Service
+@Transactional
 public class ShoppingCartService {
 	
 	@Autowired
 	ShoppingCartDao shoppingCartDao ;
+	
+	
 	
 	public void insert(Integer productId) throws SQLException {
 		shoppingCartDao.insert(productId);
